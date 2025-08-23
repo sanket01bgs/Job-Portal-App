9455eb06fab0e819c17aa9e59ef789b443a845f7
sanket01bgs <sanket8298bgs@gmail.com>
2025-08-23 08:32:52 +0530

    Job Portal

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -19,3 +19,46 @@ export const getAllJobs = async (req, res, next) => {
     jobs,
   });
 };
+
+// *****UPdate job******
+export const updateJobs = async (req, res, next) => {
+  const { id } = req.params;
+  const { company, position } = req.body;
+  //validation
+  if (!company || !position) {
+    next("Please provide all fields");
+  }
+  //find job
+  const job = await jobsModel.findOne({ _id: id });
+  //validation
+  if (!job) {
+    next(`no job found with this id ${id}`);
+  }
+  if (!req.user.userId === job.createdBy.toString()) {
+    next("You are not authorised to update this job");
+    return;
+  }
+  const updateJob = await jobsModel.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  //response
+  res.status(200).json({ updateJob });
+};
+
+// ********Delete Job *********
+export const deleteJobController = async (req, res, next) => {
+  const { id } = req.params;
+  //find job
+  const job = await jobsModel.findOne({ _id: id });
+  //validation
+  if (!job) {
+    next(`no job found with this ${id}`);
+  }
+  if (!req.user.userId === job.createdBy.toString()) {
+    next("You are not authorised to delete this job");
+    return;
+  }
+  await job.deleteOne();
+  res.status(200).json({ message: "Success, Job Deleted!" });
+};
--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -2,7 +2,9 @@ import express from "express";
 import userAuth from "../middlewares/authMiddleware.js";
 import {
   createJobController,
+  deleteJobController,
   getAllJobs,
+  updateJobs,
 } from "../controllers/jobsController.js";
 
 const router = express.Router();
@@ -13,5 +15,11 @@ router.post("/create-job", userAuth, createJobController);
 
 //GET job || GET
 router.get("/get-job", userAuth, getAllJobs);
+
+//Update job || PUT or PATCH
+router.patch("/update-job/:id", userAuth, updateJobs);
+
+//Delete job || Delete
+router.delete("/delete-job/:id", userAuth, deleteJobController);
 
 export default router;
